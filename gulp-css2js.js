0c8ace2924229c7b042c23d2192cd5b4b71173c9
Tyler Akins <fidian@rumkin.com>
2014-05-01 07:41:54 -0500

    Initial commit

---
+++ gulp-css2js.js
@@ -0,0 +1,181 @@
+/**
+ * Embed CSS into JavaScript
+ *
+ * This will take your CSS and convert it into JavaScript.  When executed,
+ * the resulting code will generate a <script> element and put the CSS text
+ * into it.  Instead of having an external file for your necessary styles,
+ * they are embedded into your library.
+ *
+ * As an exercise and possibly as an example for other Gulp developers,
+ * this plugin supports both buffers and streams.
+ */
+
+(function () {
+    'use strict';
+
+    var gulpUtil, prefixBuffer, suffixBuffer, through2;
+
+    /**
+     * @typedef {Object} gulpCss2js~options
+     * @property {string} remainder Leftover newlines from last write
+     * @property {boolean} [splitOnNewline=true]
+     * @property {boolean} [trimSpacesBeforeNewline=true]
+     * @property {boolean} [trimTrailingNewline=false]
+     */
+
+    /**
+     * Escapes content to be used in the middle of a JavaScript string.
+     * This string will be surrounded by double quotes elsewhere, but this
+     * is important because we need to escape double quotes in the buffers.
+     *
+     * @param {Buffer} bufferIn
+     * @param {gulpCss2js~options} options
+     * @param {boolean} trimTrailingNewlineDefault
+     * @return {Buffer}
+     */
+    function escapeBuffer(bufferIn, encoding, options) {
+        var newlines, str;
+
+        try {
+            str = bufferIn.toString(encoding);
+        } catch (e) {
+            str = bufferIn.toString();
+            encoding = 'utf8';
+        }
+
+        /* If there was a chunk left over from a previous write, add it back.
+         * This portion of code should only get executed when:
+         *     a stream is being processed
+         *     a chunk ends in a newline
+         *     another chunk comes after it
+         */
+        if (options.remainder) {
+            str = options.remainder + str;
+            options.remainder = '';
+        }
+
+        // Escape backslashes and double quotes
+        str = str.replace(/\\/g, '\\\\');
+        str = str.replace(/"/g, '\\"');
+
+        // Newline conversion
+        str = str.replace(/(\r?\n|\r)/g, '\n');
+
+        // Remove spaces before newlines
+        if (options.trimSpacesBeforeNewline) {
+            str = str.replace(/[\t ]*\n/g, '\n');
+        }
+
+        // Trim the last newline or convert it so it is preserved properly
+        if (options.trimTrailingNewline) {
+            newlines = str.match(/\n*$/);
+
+            if (newlines && newlines[0]) {
+                options.remainder = newlines[0];
+                str = str.replace(/\n*$/, '');
+            }
+        } else {
+            /* Prevent the last line from looking like this when encoded:
+             *    ".css { display: block }\n" +
+             *    ""
+             */
+            str = str.replace(/\n$/, '\\n');
+        }
+
+        // Break on newlines
+        if (options.splitOnNewline) {
+            str = str.replace(/\n/g, '\\n" +\n"');
+        } else {
+            str = str.replace(/\n/g, '\\n');
+        }
+
+        return new Buffer(str, encoding);
+    }
+
+    /**
+     * Uses Through2 to create a stream translation that converts CSS
+     * into JavaScript.
+     *
+     * @param {gulpCss2js~options} options
+     * @return {Stream}
+     */
+    function convertStream(options) {
+        var outStream;
+
+        outStream = through2(function (chunk, encoding, callback) {
+            this.push(escapeBuffer(chunk, encoding, options));
+            callback();
+        }, function (callback) {
+            this.push(suffixBuffer);
+            callback();
+        });
+        outStream.push(prefixBuffer);
+
+        return outStream;
+    }
+
+    gulpUtil = require('gulp-util');
+    through2 = require('through2');
+    prefixBuffer = new Buffer('(function (doc, cssText) {\n' +
+        '    var styleEl = doc.createElement("style");\n' +
+        '    doc.getElementsByTagName("head")[0].appendChild(styleEl);\n' +
+        '    if (styleEl.styleSheet) {\n' +
+        '        if (!styleEl.styleSheet.disabled) {\n' +
+        '            styleEl.styleSheet.cssText = cssText;\n' +
+        '        }\n' +
+        '    } else {\n' +
+        '        try {\n' +
+        '            styleEl.innerHTML = cssText;\n' +
+        '        } catch (ignore) {\n' +
+        '            styleEl.innerText = cssText;\n' +
+        '        }\n' +
+        '    }\n' +
+        '}(document, "', 'utf8');
+    suffixBuffer = new Buffer('"));\n', 'utf8');
+
+    module.exports = function (options) {
+        /**
+         * Default a parameter
+         *
+         * @param {string} key
+         * @param {*} fallbackValue Value to use if current is undefined
+         * @return {*}
+         */
+        function fallback(key, fallbackValue) {
+            if (options[key] === undefined) {
+                options[key] = fallbackValue;
+            }
+        }
+
+
+        options = options || {};
+        options.remainder = '';
+        fallback('splitOnNewline', true);
+        fallback('trimSpacesBeforeNewline', true);
+        fallback('trimTrailingNewline', true);
+
+        return through2.obj(function (file, encoding, callback) {
+            if (file.isBuffer()) {
+                file.contents = Buffer.concat([
+                    prefixBuffer,
+                    escapeBuffer(file.contents, encoding, options),
+                    suffixBuffer
+                ]);
+                file.path = gulpUtil.replaceExtension(file.path, ".js");
+            } else if (file.isStream()) {
+                file.contents = file.contents.pipe(convertStream(options));
+                file.path = gulpUtil.replaceExtension(file.path, ".js");
+            } else if (!file.isNull()) {
+                // Not sure what this could be, but future-proofing the code.
+                throw new gulpUtil.PluginError('gulp-css2js', 'Unhandled file source type.');
+            }
+
+            this.push(file);
+            return callback();
+        });
+    };
+
+    // Export these too for easier testing
+    module.exports.prefixBuffer = prefixBuffer;
+    module.exports.suffixBuffer = suffixBuffer;
+}());
---
+++ test/unit.js
@@ -0,0 +1,218 @@
+/**
+ * Gulp plugins really aught to have tests.  Here's some great tests for
+ * gulp-css2js.
+ */
+/*global afterEach, beforeEach, describe, it*/
+
+'use strict';
+
+var Assert, css2js, gulpUtil, stream;
+
+Assert = require('assert');
+css2js = require('../');
+gulpUtil = require('gulp-util');
+stream = require('stream');
+
+function runThroughStream(expected, srcFile, options, done) {
+    var stream;
+
+    stream = css2js(options);
+    stream.on("data", function (newFile) {
+        var buffer;
+
+        Assert.equal(newFile.path, expected.path);
+        Assert.equal(newFile.cwd, expected.cwd);
+        Assert.equal(newFile.base, expected.base);
+
+        if (newFile.isStream()) {
+            // Convert a stream into a buffer and test when it's done
+            buffer = '';
+            newFile.contents.on('data', function (chunk, encoding) {
+                buffer += chunk.toString(encoding);
+            });
+            newFile.contents.on('end', function () {
+                Assert.equal(buffer, expected.contents.toString());
+                done();
+            });
+        } else if (expected.contents === null) {
+            Assert.equal(newFile.contents, expected.contents);
+            done();
+        } else {
+            Assert.equal(newFile.contents.toString(), expected.contents.toString());
+            done();
+        }
+    });
+    stream.write(srcFile);
+    stream.end();
+}
+
+function makeFile(contents, path) {
+    if (typeof contents === 'string') {
+        contents = new Buffer(contents, 'utf8');
+    }
+
+    return new gulpUtil.File({
+        path: path || "test/styles/testing.css",
+        cwd: "test/",
+        base: "test",
+        contents: contents
+    });
+}
+
+function makeEncodedFile(contents) {
+    contents = css2js.prefixBuffer.toString() + contents + css2js.suffixBuffer.toString();
+
+    return makeFile(contents, "test/styles/testing.js");
+}
+
+describe('gulp-css2js', function () {
+    it('exported a function', function () {
+        Assert.equal('function', typeof css2js);
+    });
+
+    describe('null files', function () {
+        it('passes them through', function (done) {
+            runThroughStream(makeFile(null), makeFile(null), {}, done);
+        });
+    });
+    describe('buffered files', function () {
+        it('embeds CSS', function (done) {
+            runThroughStream(makeEncodedFile('.d-b { display: block }'), makeFile('.d-b { display: block }'), {}, done);
+        });
+    });
+    describe('streamed files', function () {
+        var sourceFile, streamChunks;
+
+        beforeEach(function () {
+            streamChunks = [];
+            sourceFile = makeFile('');
+            sourceFile.contents = new stream.Readable();
+            sourceFile.contents._read = function () {
+                if (streamChunks.length) {
+                    this.push(new Buffer(streamChunks.shift(), 'utf8'), 'utf8');
+                } else {
+                    this.push(null);
+                }
+            };
+        });
+        it('embeds CSS', function (done) {
+            streamChunks.push('.d-b {');
+            streamChunks.push(' display: blo');
+            streamChunks.push('ck }');
+            runThroughStream(makeEncodedFile('.d-b { display: block }'), sourceFile, {}, done);
+        });
+        it('compensates for newlines at the end of chunks', function (done) {
+            streamChunks.push("line1 {}\n");
+            streamChunks.push("line2 {}\n");
+            runThroughStream(makeEncodedFile('line1 {}\\n" +\n"line2 {}'), sourceFile, {
+                trimTrailingNewline: true
+            }, done);
+        });
+    });
+    describe('options', function () {
+        describe('splitOnNewline', function () {
+            var disabledOutput, enabledOutput, input;
+
+            beforeEach(function () {
+                input = "body { margin: 0 }\nh1 { padding-top: 10px }\n";
+                disabledOutput = 'body { margin: 0 }\\nh1 { padding-top: 10px }\\n';
+                enabledOutput = 'body { margin: 0 }\\n" +\n"h1 { padding-top: 10px }\\n';
+            });
+            it('defaults to true', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {
+                    trimTrailingNewline: false
+                }, done);
+            });
+            it('does not break lines if disabled', function (done) {
+                runThroughStream(makeEncodedFile(disabledOutput), makeFile(input), {
+                    splitOnNewline: false,
+                    trimTrailingNewline: false
+                }, done);
+            });
+            it('breaks on newlines but avoids empty string concatenation at the end when enabled', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {
+                    splitOnNewline: true,
+                    trimTrailingNewline: false
+                }, done);
+            });
+        });
+        describe('trimSpacesBeforeNewline', function () {
+            var disabledOutput, enabledOutput, input;
+
+            beforeEach(function () {
+                input = "a, \t \ndiv { display: block }    \n";
+                disabledOutput = 'a, \t \\ndiv { display: block }    ';
+                enabledOutput = 'a,\\ndiv { display: block }';
+            });
+            it('defaults to true', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {
+                    splitOnNewline: false
+                }, done);
+            });
+            it('does not trim spaces if disabled', function (done) {
+                runThroughStream(makeEncodedFile(disabledOutput), makeFile(input), {
+                    splitOnNewline: false,
+                    trimSpacesBeforeNewline: false
+                }, done);
+            });
+            it('trims spaces when enabled', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {
+                    splitOnNewline: false,
+                    trimSpacesBeforeNewline: true
+                }, done);
+            });
+        });
+        describe('trimTrailingNewline', function () {
+            var disabledOutput, enabledOutput, input;
+
+            beforeEach(function () {
+                input = "div { display: block }\n";
+                disabledOutput = 'div { display: block }\\n';
+                enabledOutput = 'div { display: block }';
+            });
+            it('defaults to true', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {}, done);
+            });
+            it('does not trim newline if disabled', function (done) {
+                runThroughStream(makeEncodedFile(disabledOutput), makeFile(input), {
+                    trimTrailingNewline: false
+                }, done);
+            });
+            it('trims newline when enabled', function (done) {
+                runThroughStream(makeEncodedFile(enabledOutput), makeFile(input), {
+                    trimTrailingNewline: true
+                }, done);
+            });
+        });
+    });
+    describe('escaping', function () {
+        it('escapes as necessary', function (done) {
+            var escaped, unescaped;
+
+            unescaped = '';
+            escaped = '';
+
+            // Double quotes - yes
+            unescaped += '"';
+            escaped += '\\"';
+
+            // Single quotes - no
+            unescaped += "'";
+            escaped += "'";
+
+            // Newlines are converted - Unix, DOS, old Mac
+            unescaped += "\n \r\n \r";
+            escaped += "\\n \\n \\n";
+
+            // Tabs - no
+            unescaped += "\t";
+            escaped += "\t";
+
+            runThroughStream(makeEncodedFile(escaped), makeFile(unescaped), {
+                splitOnNewline: false,
+                trimSpacesBeforeNewline: false,
+                trimTrailingNewline: false
+            }, done);
+        });
+    });
+});
